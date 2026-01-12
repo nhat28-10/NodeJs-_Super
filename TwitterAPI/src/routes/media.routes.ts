93b0a89b5b952a9dc6847fdb0e1fa9559b47b709
nhat28-10 <nhatnguyen281003@gmail.com>
2026-01-12 11:52:11 +0700

    Feat:Filter upload file image

--- TwitterAPI/src/routes/media.routes.ts
+++ TwitterAPI/src/routes/media.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { uploadSingleImageController } from "~/controllers/medias.controller";
+import { warpRequestHandler } from "~/utils/handlers";
 
 const mediasRouter = Router()
-mediasRouter.post('/upload-image', uploadSingleImageController)
+mediasRouter.post('/upload-image', warpRequestHandler(uploadSingleImageController))
 export default mediasRouter
