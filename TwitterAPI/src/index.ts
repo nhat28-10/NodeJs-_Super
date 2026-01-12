43d4f707146cbdd36abcbb7641f3c5915f20424c
nhat28-10 <nhatnguyen281003@gmail.com>
2026-01-12 11:28:40 +0700

    Feat:Create folder upload

--- TwitterAPI/src/index.ts
+++ TwitterAPI/src/index.ts
@@ -3,12 +3,14 @@ import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/media.routes'
+import { initFolder } from './utils/file'
 databaseService.connect()
 const app = express()
 const port = 3000
+initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
-app.use('/media', mediasRouter)
+app.use('/medias', mediasRouter)
 
 app.use(defaultErrorHandler)
 app.listen(port, () => {
