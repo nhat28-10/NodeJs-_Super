104f5d131c22c6e391d139be6d55cad90909e45a
nhat28-10 <nhatnguyen281003@gmail.com>
2026-02-02 11:38:59 +0700

    Feat:Create Indexes for Video Status, Follower, RefreshToken in MongoDB

--- TwitterAPI/src/index.ts
+++ TwitterAPI/src/index.ts
@@ -14,6 +14,9 @@ config()
 databaseService.connect().then(
   () => {
     databaseService.indexUsers()
+    databaseService.indexRefreshTokens()
+    databaseService.indexVideoStatus()
+    databaseService.indexFollowers()
   }
 )
 const app = express()
